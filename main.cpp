ea0b69bbe85a0791544891cb8c14d78798c8f6a7
Shiva Kumar <kumar.shiva1807@gmail.com>
2019-07-29 23:24:58 +0530

    Updated CPP file

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 
 using namespace std;
-int add(int x,int y);
 
 int main()
 {   cout << "Enter any two numbers to be added" << endl;
     int a,b,sum;
     cin>>a>>b;
-    sum=add(a,b);
+    sum=a+b;
     cout<<"Summation of Numbers= "<<sum<<endl;
 
     return 0;
 }
-int add(int x,int y)
-{
-    return x+y;
-}
+
